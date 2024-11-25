de4c80fb766f2e5cf28fdfcec72bed9a2596ad7b
umme006 <ummehanneecassim@gmail.com>
2024-11-25 11:52:19 +0200

    added new files

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haunted_house
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Haunted House!");
+            Console.WriteLine("You find yourself standing in front of a dark, eerie house. Do you dare to enter?");
+            Console.WriteLine("Type 'yes' to enter or 'no' to run away.");
+
+            string choice = Console.ReadLine().ToLower();
+
+            if (choice == "yes")
+            {
+                EnterHouse();
+            }
+            else if (choice == "no")
+            {
+                Console.WriteLine("You chose to run away. Maybe next time you’ll be brave enough!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Game over.");
+            }
+        }
+
+        static void EnterHouse()
+        {
+            Console.WriteLine("You enter the haunted house. It's dark and silent.");
+            Console.WriteLine("Suddenly, you hear a whisper. 'Who dares enter my domain?'");
+            Console.WriteLine("Do you explore the house or run away? (explore/run)");
+
+            string choice = Console.ReadLine().ToLower();
+
+            if (choice == "explore")
+            {
+                ExploreHouse();
+            }
+            else if (choice == "run")
+            {
+                Console.WriteLine("You chose to run away. The ghost lets out a chilling laugh as you escape.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Game over.");
+            }
+        }
+
+        static void ExploreHouse()
+        {
+            Console.WriteLine("You decide to explore the house. As you walk through the hallway, you feel a cold breeze.");
+            Console.WriteLine("You see two doors. Do you enter the left door or the right door? (left/right)");
+
+            string choice = Console.ReadLine().ToLower();
+
+            if (choice == "left")
+            {
+                Console.WriteLine("You enter the left door and find a room filled with ancient books. You start reading and get lost in the stories. The ghost approves.");
+            }
+            else if (choice == "right")
+            {
+                Console.WriteLine("You enter the right door and fall into a trap. The ghost captures you. Game over.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Game over.");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Haunted house")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Haunted house")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("501e23c4-cc79-44bd-81fb-6a499e88ab1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
